41d52fc5901b41b0860b7ae06771a46127cc73cb
João Marcos <JoaoMarcos_rgt@hotmail.com>
2019-10-23 17:21:38 -0300

    Cmt

--- Correios/Correios/Scripts/Utils/scripts.js
+++ Correios/Correios/Scripts/Utils/scripts.js
@@ -1,1 +1,28 @@
-﻿
+﻿$(document).ready(function () {
+    $('#botaoCalc').click(function () {
+        var obj = {
+            nCdEmpresa: "",
+            sDsSenha: "",
+            sCepOrigem: $("#cepReme").val(),
+            sCepDestino: $("#cepDest").val(),
+            nVlPeso: $("#peso").val(),
+            nCdFormato: $("#formato").val(),
+            nVlComprimento: $("#comprimento").val(),
+            nVlAltura: $("#altura").val(),
+            nVlLargura: $("#largura").val(),
+            sCdMaoPropria: "",
+            nVlValorDeclarado:"",
+            sCdAvisoRecebimento:"",
+            nCdServico:"",
+            nVlDiametro: $("#diametro").val()
+        };
+
+        $.post("http://usysweb.com.br/api/correiosambev.php?text", obj, function (data) {
+            alert(JSON.parse(data).cServico.Valor);
+            alert(JSON.parse(data).cServico.PrazoEntrega);
+
+            //alert("#valor").text("R$ " + JSON.parse(data).cServico.Valor);
+            //alert("#prazo").text(JSON.parse(data).cServico.PrazoEntrega + " Dias");
+        });
+    });
+});
